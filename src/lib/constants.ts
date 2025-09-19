0ac693419970eea99625a69ba8568eb7a94e4634
disavowed913 <61739808+ScriptKiddie913@users.noreply.github.com>
2025-09-20 01:51:56 +0530

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 // src/lib/constants.ts
-export const REGISTRY_ADDRESS = "0x2b8E5E41D4aAfc8E295c264EC52f566FF6Df95A9";
+export const REGISTRY_ADDRESS = "0x0d0A38A501B2AD98248eD7E17b6025D9a55F5044";
 export const SEPOLIA_EXPLORER_TX = "https://sepolia.etherscan.io/tx/";
